b6cda9a674bf68d7099d041751bbee1bbba762bb
Carl-Erik Kopseng <carlerik@gmail.com>
2023-08-04 13:14:37 +0200

    Pure ESM (not TS) working with Quibble

---
+++ src/main.spec.js
@@ -0,0 +1,25 @@
+import "./init.js";
+
+import { expect } from "chai";
+import quibble from "quibble";
+import sinon from "sinon";
+
+const sandbox = sinon.createSandbox();
+
+describe("main module", () => {
+  let mocked, main;
+
+  before(async () => {
+    mocked = sandbox.fake.returns("mocked");
+    await quibble.esm("./other.js", { toBeMocked: mocked });
+    const mainModule = await import("./main.js");
+    main = mainModule.main;
+    console.log("ferdig setup");
+  });
+
+  it("should mock", () => {
+    console.log("test starts");
+    main();
+    expect(mocked.called).to.be.true;
+  });
+});
