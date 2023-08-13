7d17d4b920b1a81426be4cf102e8a43e02c54427
Carl-Erik Kopseng <carlerik@gmail.com>
2023-08-14 00:08:21 +0200

    Pure DI version

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -1,15 +1,22 @@
-import sinon from "sinon";
 import "./init";
+
 import * as Other from "./other";
-import { main } from "./main";
+
 import { expect } from "chai";
+import { main } from "./main";
+import sinon from "sinon";
 
 const sandbox = sinon.createSandbox();
 
 describe("main", () => {
   let mocked;
+  let original = Other.toBeMocked;
+
+  after(() => Other._setToBeMocked(original))
+
   it("should mock", () => {
-    mocked = sandbox.stub(Other, "toBeMocked").returns("mocked");
+    mocked = sandbox.stub().returns("mocked");
+    Other._setToBeMocked(mocked)
     main();
     expect(mocked.called).to.be.true;
   });
--- src/other.ts
+++ src/other.ts
@@ -1,3 +1,9 @@
-export function toBeMocked() {
+function _toBeMocked() {
   return "I am the original function";
+}
+
+export let toBeMocked = _toBeMocked
+
+export function _setToBeMocked(mockImplementation){
+    toBeMocked = mockImplementation
 }
